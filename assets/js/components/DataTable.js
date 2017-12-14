f30b88526d45353ad9483cadab28793f61c89230
Rostislav Khaniukov <rostyslav.khanyukov@ms.sumdu.edu.ua>
2017-12-14 13:38:51 +0200

    feat(table): add table structure

--- assets/js/components/DataTable.js
+++ assets/js/components/DataTable.js
@@ -27,6 +27,16 @@ export default class DataTable extends HTMLElement {
                   background: #b00000;
                 }
                 
+                .btn.delete.row {
+                  margin-top: 3px;
+                  margin-right: -2px;
+                }
+                
+                .btn.delete.column {
+                  margin-bottom: -2px;
+                  margin-left: 3px;
+                }
+                
                 .btn.delete:hover {
                   background: #c64e4d;
                 }
@@ -35,11 +45,60 @@ export default class DataTable extends HTMLElement {
                   background: #f5a214;
                 }
                 
+                .btn.add.row {
+                  margin-left: 3px;
+                  margin-top: -2px;
+                }
+                
+                .btn.add.column {
+                  margin-left: -2px;
+                  margin-top: 3px;
+                }
+                
                 .btn.add:hover {
                   background: #f5c24f;
                 }
+                
+                .table {
+                  border: 1px #4cabe3 solid;
+                  text-align: center;
+                }
+                
+                .table td {
+                  background-color: #4cabe3;
+                  width: 50px;
+                  height: 50px;
+                  color: white;
+                  font-size: 12px;
+                }
+                
+                .btn-cell {
+                  vertical-align: top;
+                }
             </style>
-            <table class="data-table-wrapper"></table>
+            <table class="data-table-wrapper">
+                <tr>
+                    <td></td>
+                    <td class="btn-cell"><button id="remove-column-btn" class="btn delete column"><span>-</span></button></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td class="btn-cell"><button id="remove-row-btn" class="btn delete row"><span>-</span></button></td>
+                    <td>
+                        <table class="table">
+                            <tr>
+                                <td>[0, 0]</td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td class="btn-cell"><button id="add-column-btn" class="btn add column"><span>+</span></button></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="btn-cell"><button id="add-row-btn" class="btn add row"><span>+</span></button></td>
+                    <td></td>
+                </tr>
+            </table>
         `;
 
         shadow.appendChild(this.div);
