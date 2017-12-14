685cf8c1734608844dc24dea6e9b51d81a9c94c4
Rostislav Khaniukov <rostyslav.khanyukov@ms.sumdu.edu.ua>
2017-12-14 13:36:01 +0200

    feat(buttons): add style for buttons

--- assets/js/components/DataTable.js
+++ assets/js/components/DataTable.js
@@ -8,7 +8,37 @@ export default class DataTable extends HTMLElement {
         this.columnCount = 4;
 
         this.div.innerHTML = `
-            <style></style>
+            <style>
+                .btn {
+                  width: 50px;
+                  height: 50px;
+                  border: none;
+                  color: #fff;
+                  -webkit-transition: background-color 1s ease-out;
+                  transition: background-color 1s ease-out;
+                }
+                
+                .btn span {
+                  font-size: 20px;
+                  line-height: 1;
+                }
+                
+                .btn.delete {
+                  background: #b00000;
+                }
+                
+                .btn.delete:hover {
+                  background: #c64e4d;
+                }
+                
+                .btn.add {
+                  background: #f5a214;
+                }
+                
+                .btn.add:hover {
+                  background: #f5c24f;
+                }
+            </style>
             <table class="data-table-wrapper"></table>
         `;
 
